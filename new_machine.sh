07c3e502873197d6abed7bf095e0fbaafc2af8ba
danielwolters <dnlw2001@gmail.com>
2021-06-13 16:37:39 +0200

    Update new_machine.sh
    push 2

--- new_machine.sh
+++ new_machine.sh
@@ -1,2 +1,11 @@
 #! /bin/bash
 
+# Install saltstack
+add-apt-repository ppa:saltstack/salt -y
+apt-get update -y
+apt-get install salt-minion -y
+apt-get upgrade -y
+
+# Set salt master location and start minion
+sed -i 's/#master: salt/master: 10.0.6.54/' /etc/salt/minion
+salt-minion -d
